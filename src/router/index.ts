b31366418c8abf1a70e9c8256e756bd8f6ac5617
yeoman wang <yuming.wang423@gmail.com>
2024-11-09 16:34:10 +0800

    first commit

---
+++ src/router/index.ts
@@ -0,0 +1,41 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '../views/HomeView.vue'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/dashboard',
+      name: 'dashboard',
+      children: [
+        {
+          path: 'profile',
+          component: HomeView,
+        },
+        {
+          path: 'posts',
+          component: HomeView,
+        },
+
+      ]
+    },
+    {
+      path: '/personal',
+      name: 'personal',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import('../views/AboutView.vue'),
+    },
+    {
+      path: '/others',
+      name: 'others',
+      component: HomeView,
+    },
+    // { path: '/:pathMatch(.*)*', name: 'NotFound', component: NotFound },
+    // // 将匹配以 `/user-` 开头的所有内容，并将其放在 `route.params.afterUser` 下
+    // { path: '/user-:afterUser(.*)', component: UserGeneric },
+  ],
+})
+
+export default router
---
+++ src/views/SideBar.vue
@@ -0,0 +1,59 @@
+<template>
+  <n-space vertical>
+    <n-switch v-model:value="collapsed" />
+    <n-layout has-sider>
+      <n-layout-sider
+        bordered
+        collapse-mode="width"
+        :collapsed-width="64"
+        :width="240"
+        :collapsed="collapsed"
+        show-trigger
+        @collapse="collapsed = true"
+        @expand="collapsed = false"
+      >
+        <n-menu
+          v-model:value="activeKey"
+          @update:value="navTo(activeKey)"
+          :collapsed="collapsed"
+          :collapsed-width="64"
+          :collapsed-icon-size="22"
+          :options="menuOptions"
+        />
+      </n-layout-sider>
+      <n-layout>
+        <span>内容</span>
+      </n-layout>
+    </n-layout>
+  </n-space>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref, toRefs } from 'vue'
+import { useRouter } from 'vue-router'
+
+export default defineComponent({
+  props: {
+    menuOptions: {
+      type: Array,
+      default: () => [], // 设置默认值为空数组
+    },
+  },
+  emits: ['selectOption'],
+  setup(props, ctx) {
+    const { menuOption } = toRefs(props)
+    const router = useRouter()
+    const navTo = (key) => {
+      console.log(key)
+      ctx.emit('selectOption', key)
+      router.push('/dashboard/profile/')
+    }
+    return {
+      activeKey: ref<string | null>(null),
+      collapsed: ref(true),
+      menuOption,
+      navTo,
+    }
+  },
+})
+</script>
