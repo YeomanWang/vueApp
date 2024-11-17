9f7e37a92336b4b88924a03f87cd3743fb5b5c6c
YeomanWang <90294309+YeomanWang@users.noreply.github.com>
2024-11-17 17:49:30 +0800

    feat(yeoman): add husky commitlint (#3)

---
+++ commitlint.config.cjs
@@ -0,0 +1,13 @@
+module.exports = {
+    extends: ['@commitlint/config-conventional'],
+    rules: {
+      'type-enum': [
+        2,
+        'always',
+        ['feat', 'fix', 'docs', 'style', 'refactor', 'test', 'chore'],
+      ],
+      'scope-empty': [2, 'never'],
+      'subject-case': [0],
+    },
+  };
+  
