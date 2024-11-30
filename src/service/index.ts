42e817ea97bd007dd18a28f38318e867612f310b
yeoman wang <yuming.wang423@gmail.com>
2024-11-30 22:53:43 +0800

    feat(yeoman): add scroll loading to photos page

---
+++ src/service/index.ts
@@ -0,0 +1,22 @@
+import axios from 'axios';
+
+// 创建 Axios 实例
+const apiClient = axios.create({
+  baseURL: 'http://example.com',
+  headers: {
+    'Content-Type': 'application/json',
+  },
+});
+
+// 请求拦截器：在每个请求中加上 Authorization 头
+apiClient.interceptors.request.use(config => {
+  const token = localStorage.getItem('auth_token'); // 从 localStorage 获取 token
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`; // 添加 Authorization 头
+  }
+  return config;
+}, error => {
+  return Promise.reject(error);
+});
+
+export default apiClient;
--- src/views/dashboard/HomeView.vue
+++ src/views/dashboard/HomeView.vue
@@ -2,19 +2,35 @@
 import { onMounted } from 'vue'
 import SideBar from './SideBar.vue'
 import type { MenuOption } from 'naive-ui'
+// import {
+//   BookOutline as BookIcon
+// } from '@vicons/ionicons5'
+// function renderIcon(icon: Component) {
+//   return () => h(NIcon, null, { default: () => h(icon) })
+// }
 const menuOptions: MenuOption[] = [
   {
     label: '王一一的照片',
     key: 'photos',
   },
-
+  {
+    key: 'divider-1',
+    type: 'divider',
+    props: {
+      style: {
+        marginLeft: '32px'
+      }
+    }
+  },
   {
     label: '杂七杂八',
     key: 'test',
+    // icon: renderIcon(BookIcon)
   },
   {
     label: '王一一的视频',
     key: 'video',
+    // icon: renderIcon(BookIcon)
   },
 ]
 onMounted(() => {})
